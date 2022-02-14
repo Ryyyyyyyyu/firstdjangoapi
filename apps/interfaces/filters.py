b8ad07eb34b0d1a6a7e913ab51018d3122c5fd54
Ryyyyyyyyu <2458757210@qq.com>
2022-02-14 22:37:56 +0800

    接口模块过滤字段

--- apps/interfaces/filters.py
+++ apps/interfaces/filters.py
@@ -6,6 +6,7 @@ from interfaces import models
 
 class InterfacesFilterSet(FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    project_id = filters.CharFilter(field_name='project', lookup_expr='exact')
 
     class Meta:
         model = models.InterfacesModel
